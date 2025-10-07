070cdd350915d22b4d15ed605f1fc5024b79773c
unknown <sundayfirst1st@gmail.com>
2025-10-07 08:09:21 +0700

    secss baru new again lagi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,15 @@
     <title>{{ config('app.name', 'Laravel ToDoList') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     
-    <!-- Tambahkan yield styles di sini -->
     @yield('styles')
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <nav class="navbar navbar-expand-lg navbar-custom mb-4">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">ToDoList</a>
 
@@ -31,7 +34,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     
-    <!-- Opsional: untuk script tambahan -->
     @yield('scripts')
 </body>
 </html>
