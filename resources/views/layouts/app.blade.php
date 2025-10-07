2f742066c947ca177b02f294422e3ad6c128af82
rian <riannsahh28@gmail.com>
2025-10-07 10:54:36 +0700

    ngambil seluruh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <title>{{ config('app.name', 'Laravel ToDoList') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/task-list-custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/create-taks.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/notifications.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/taks-list.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     
     <!-- Tambahkan yield styles di sini -->
